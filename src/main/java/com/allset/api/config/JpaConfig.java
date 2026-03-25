2ec47e1923fe7f40c5b83a051ec66186ca4ecebb
Overkillas <kaikepn2@gmail.com>
2026-03-25 11:32:27 -0300

    feat(config): enable JPA auditing

---
+++ src/main/java/com/allset/api/config/JpaConfig.java
@@ -0,0 +1,8 @@
+package com.allset.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {}
